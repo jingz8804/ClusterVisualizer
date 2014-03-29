12605333affbfc07517f2339e5b08e3d4992b3e7
jingz8804 <jingz8804@gmail.com>
2014-03-28 19:51:48 -0600

    change to new UI layout with new version of Shiny

--- ui.R
+++ ui.R
@@ -1,18 +1,38 @@
 library(shiny)
 
-shinyUI(pageWithSidebar(
-  headerPanel("Conditional Panels"),
-  sidebarPanel(
-    conditionalPanel(condition="input.condPanels==1",
-                     helpText("Content Panel1")),
-    conditionalPanel(condition="input.condPanels==2",
-                     helpText("Content Panel2"))
-  ),
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Panel 1", value=1),
-      tabPanel("Panel 2", value=2),
-      id="condPanels")
+# shinyUI(pageWithSidebar(
+#   headerPanel("Conditional Panels"),
+#   sidebarPanel(
+#     conditionalPanel(condition="input.condPanels==1",
+#                      helpText("Content Panel1")),
+#     conditionalPanel(condition="input.condPanels==2",
+#                      helpText("Content Panel2"))
+#   ),
+#   mainPanel(
+#     tabsetPanel(
+#       tabPanel("Panel 1", value=1),
+#       tabPanel("Panel 2", value=2),
+#       id="condPanels")
+#     )
+#   )
+# )
+
+shinyUI(fluidPage(
+    titlePanel("Conditional Panel"),
+    
+    sidebarLayout(
+      sidebarPanel(
+        conditionalPanel(condition="input.condPanels==1",
+                         helpText("Content Panel1")),
+        conditionalPanel(condition="input.condPanels==2",
+                         helpText("Content Panel2"))
+      ),
+      mainPanel(
+        tabsetPanel(
+          tabPanel("Panel 1", value=1),
+          tabPanel("Panel 2", value=2),
+          id="condPanels")
+      )
     )
   )
 )
